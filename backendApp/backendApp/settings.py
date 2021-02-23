90f5a70538466964e1f09cf8aab7d736b138647a
nishitchittora <nishitchittora2015@gmail.com>
2021-02-23 10:35:08 +0530

    secret key removed

--- backendApp/backendApp/settings.py
+++ backendApp/backendApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x2_(inzdfwj6x0#kpklg(g$=g-4w1qp!=9$s36a*n7=6-tjoek'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
